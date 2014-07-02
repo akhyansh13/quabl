acc4ae5559fd4a0b68c2e6db7fd3c49d75bc6072
akhyansh13 <akhyansh13@gmail.com>
2014-07-02 19:29:39 +0530

    Minor Fix.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
 		$(".go-back").click(function(){
 			var $t = $(this).parent().parent().parent();
 			$(this).toggle("slow",function(){
-				$t.find(".remove, .glyphicon-trash").toggle();
+				$t.find(".remove, .glyphicon-trash").toggle("200");
 			});			
 		});
 
