2432815ea32937df1b1a4c90bc7d4a77dd8f8df6
akhyansh13 <akhyansh13@gmail.com>
2014-07-04 02:40:29 +0530

    =Edited Django admin. Editing db now easier.

--- SimplerApp/admin.py
+++ SimplerApp/admin.py
@@ -1,7 +1,20 @@
+from django.db import models
 from django.contrib import admin
+from django.forms import TextInput, Textarea
 from models import Post, Simpler, UserProfile, simpler_request
 
-admin.site.register(Post)
-admin.site.register(Simpler)
+class SimplerAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea(attrs={'rows':'8', 'cols':'80'})},
+    }
+
+class PostAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea(attrs={'rows':'8', 'cols':'80'})},
+    }
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Simpler, SimplerAdmin)
 admin.site.register(UserProfile)
 admin.site.register(simpler_request)
+
