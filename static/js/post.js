9acc09f013fa47c0649ce5e9ac57b8377bcf4e3b
akhyansh13 <akhyansh13@gmail.com>
2014-10-03 23:00:45 +0530

    Minor Fixes.

--- static/js/post.js
+++ static/js/post.js
@@ -111,7 +111,6 @@ $(document).ready(function(){
 			uri = '/highlight/' + post_id + '/' + simpler_id + '/' + highlight + '/' + '0/';
 			
 			$(this).parents(".simpler-wrapper").find(".addness").removeAttr('disabled');
-			$(this).parents(".simpler-wrapper").find(".reqsimp").removeAttr('disabled');			
 			$(this).parents(".simpler-wrapper").find(".addhigh").attr('href', uri);
 			$(this).parents(".simpler-wrapper").find(".addness").attr('class', "btn btn-success addness");
 			$(this).parents(".simpler-wrapper").find(".reqsimp").attr('value', highlight);
@@ -119,7 +118,6 @@ $(document).ready(function(){
 		}
 		else {			
 			$(this).parents(".simpler-wrapper").find(".addness").attr('disabled', "disabled");
-			$(this).parents(".simpler-wrapper").find(".reqsimp").attr('disabled', "disabled");
 			$(this).parents(".simpler-wrapper").find(".addhigh").removeAttr('href');
 			$(this).parents(".simpler-wrapper").find(".addness").attr('class', "btn btn-default addness");
 			$(this).parents(".simpler-wrapper").find(".reqsimp").removeAttr('value');
