a3f74cecde1c8bd7a4ca88f0d01d4d347a1dff0b
adityamaharana7 <adityamaharana7@gmail.com>
2014-11-01 19:50:42 +0530

    Solved Re-Highlighting

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$('.dropdown-menu').each(function(){
+	$('#dropdown-notifications').each(function(){
 		$(this).attr('style', "max-width:450px; min-width:450px; width:450px; min-height:" + screen.height + "px; max-height:" + screen.height + "px; height:450" + screen.height + "px;");
 	});
 
