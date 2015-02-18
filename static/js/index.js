acf771e188ec92d45a07c9204358f831aac7d5a1
Ubuntu <ubuntu@ip-172-31-16-177.us-west-2.compute.internal>
2015-02-18 12:58:39 +0000

    Minor.

--- static/js/index.js
+++ static/js/index.js
@@ -110,6 +110,7 @@ $(document).ready(function(){
 		$("#afeed").hide();
 		$("#ocont").hide();
 		$("#linkspage").show();
+		$("#contentind").hide("fast");
 	});
 
 	$(".viewcont").click(function(){
